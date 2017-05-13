48753055563b5c8760887a94dc28826236006e0e
unix2315 <unix.2315@gmail.com>
2017-05-13 12:35:39 +0300

    modify settings

--- redis_pr/wsgi.py
+++ redis_pr/wsgi.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatserver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redis_pr.settings')
 
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
