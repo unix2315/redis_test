39756611c2eaea624195c7aec557800a6bb149df
unix2315 <unix.2315@gmail.com>
2017-05-13 11:22:47 +0300

    check redis settings

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-redis-server
+redis-cli config get stop-writes-on-bgsave-error
+redis-cli config get save
+redis-cli BGSAVE
+redis-cli config get logfile
+redis-cli config get dir
+ls -l /var/log/redis/
+ls -l /var/lib/redis/
